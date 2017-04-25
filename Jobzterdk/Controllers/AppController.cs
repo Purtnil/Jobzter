cedd695f48d3d57f47182f0f543e2105440debe0
juli502b <juli502b@VIDB6NFH12>
2017-04-25 13:07:13 +0200

    hest

---
+++ Jobzterdk/Controllers/AppController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Helpers;
+using System.Web.Http;
+using System.Web.Security;
+using RepoJZ;
+
+namespace Jobzterdk.Controllers
+{
+    public class AppController : ApiController
+    {
+        BrugerFac bf = new BrugerFac();
+
+        [Route("api/App/Login/{email}/{adgangskode}")]
+        [HttpGet]
+        public Bruger Login(string email, string adgangskode)
+        {
+            Bruger b = bf.Login(email, adgangskode);
+
+            return b;
+        }
+    }
+}
---
+++ RepoJZ/Models/Sprog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepoJZ
+{
+    public class Sprog
+    {
+        public int ID { get; set; }
+        public int CVID { get; set; }
+        public int SprogetID { get; set; }
+    }
+}
