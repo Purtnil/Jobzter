e172547ad645a181f541ad9a3e2207306c47ea37
sven1106 <sven1106@VIDG416G72>
2017-04-27 11:44:40 +0200

    Login

--- Jobzterdk/Controllers/AppController.cs
+++ Jobzterdk/Controllers/AppController.cs
@@ -22,5 +22,95 @@ namespace Jobzterdk.Controllers
 
             return b;
         }
+
+
+
+        [Route("api/App/PostTest")]
+        [HttpPost]
+        public string PostTest(string email)
+        {
+                return "string";
+
+        }
+
+
+
+
+
+
+
+        [Route("api/App/GlemtAdg/{email}")]
+        [HttpPost]
+        public string GlemtAdg(string email)
+        {
+            Mail m = new Mail("smtp.gmail.com", "webitsven1106@gmail.com", "webitsven1106", "tw5mc7z8vc", 587);
+            if (bf.UserExist(email))
+            {
+                Uploader uploader = new Uploader();
+                string nyAdgangskode = uploader.GenRnd(8);
+
+                bf.UpdateAdgangskode(email, Crypto.Hash(nyAdgangskode));
+
+                m.Send("Ny adgangskode", nyAdgangskode, email);
+
+                return "Du vil om kort tid modtage en mail med en ny adgangskode!";
+
+            }
+            else
+            {
+               return "Brugeren findes ikke";
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //[Route("api/App/GlemtAdg/{email}")]
+        //[HttpPost]
+        //public string GlemtAdg(string email)
+        //{
+        //    List<Bruger> b = bf.GetBy("Email", email);
+        //    if (b.Count > 0) //GetBy(Feltet, Værdi) Count tæller hvor mange rækker der er i listen.
+        //    {
+        //        Uploader uploader = new Uploader();
+        //        Mail m = new Mail("smtp.gmail.com", "webitsven1106@gmail.com", "webitsven1106", "tw5mc7z8vc", 587); //ÆNdre til jobzters egen mail informationer
+        //        string nyAdgangskode = uploader.GenRnd(8);
+
+        //        bf.UpdateAdgangskode(email, Crypto.Hash(nyAdgangskode));
+
+        //        m.Send("Ny adgangskode", nyAdgangskode, email);
+        //        return "Vi har sendt et nyt password til din email";
+        //    }
+        //    else
+        //    {
+        //        return "Brugeren findes ikke!";
+        //    }
+        //}
     }
 }
--- Jobzterdk/Controllers/HomeController.cs
+++ Jobzterdk/Controllers/HomeController.cs
@@ -4,15 +4,18 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using RepoJZ;
+using System.Web.Helpers;
 
 namespace Jobzterdk.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Home
+        BrugerFac bf = new BrugerFac();
         public ActionResult Index()
         {
             return View();
         }
+
     }
 }
