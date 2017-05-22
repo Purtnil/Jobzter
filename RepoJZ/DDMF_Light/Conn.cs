d9d8836b3430a6fea0dc412f05f8bc78bc59642f
sven1106 <sven1106@VIDG416G72>
2017-05-22 10:24:11 +0200

    Backend

--- RepoJZ/DDMF_Light/Conn.cs
+++ RepoJZ/DDMF_Light/Conn.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.SqlClient;
+﻿using System;
+using System.Data.SqlClient;
 
 namespace RepoJZ
 {
@@ -17,5 +18,28 @@ namespace RepoJZ
             cn.Open();
             return cn;
         }
+        /// <summary>
+        /// Bruges til at tjekke om der er forbindelse til databasen
+        /// C#: var t = RepoAM.Conn.Check();
+        /// Razor: @RepoAM.Conn.Check()
+        /// </summary>
+        /// <returns>Retunere true eller false</returns>
+        public static bool Check()
+        {
+            bool t = true;
+            var cn = GetCon();
+
+            try
+            {
+                cn.Open();
+            }
+            catch (Exception)
+            {
+                t = false;
+            }
+
+            return t;
+        }
     }
+
 }
